f8e62fc6af320a412a2dabe80158df13e85361e9
@nanami-skrm <skrm773@gmail.com>
2020-09-08 21:32:35 +0900

    [Finish]3-9

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,44 @@
+class TodolistsController < ApplicationController
+
+  def new
+  	@list = List.new
+  end
+
+   def create
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    redirect_to todolist_path(list.id)
+  end
+
+  def index
+  	@lists = List.all
+  end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  def edit
+  	@list = List.find(params[:id])
+  end
+
+  def update
+  	list = List.find(params[:id])
+  	list.update(list_params)
+  	redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+  	list = List.find(params[:id])  # データ(レコード)を1件取得
+    list.destroy
+    redirect_to todolists_path  # List一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'dbeb7689959332e9e7e578d6d54fc7dd64d718f4d91070cea422e39a43c1d52c61cfe72ee5f9e2f0ce4a8c471097dc6897a003193f5b6a0d332aceef0ae6445a'
